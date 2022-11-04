092437f5d0ef9413e6e563da1e61adcd6be21aa3
Chris Hain <c.hain@f5.com>
2022-11-04 15:14:17 -0700

    unscrew xpkg publish

--- build/makelib/xpkg.mk
+++ build/makelib/xpkg.mk
@@ -80,10 +80,10 @@ $(foreach x,$(XPKGS),$(eval $(call xpkg.build.targets,$(x))))
 define xpkg.release.targets
 xpkg.release.publish.$(1).$(2):
 	@$(INFO) Pushing package $(1)/$(2):$(VERSION)
-	# @$(UP) xpkg push \
-	# 	$(foreach p,$(XPKG_LINUX_PLATFORMS),--package $(XPKG_OUTPUT_DIR)/$(p)/$(2)-$(VERSION).xpkg ) \
-	# 	$(1)/$(2):$(VERSION) || $(FAIL)
-	# @$(OK) Pushed package $(1)/$(2):$(VERSION)
+	@$(UP) xpkg push \
+		$(foreach p,$(XPKG_LINUX_PLATFORMS),--package $(XPKG_OUTPUT_DIR)/$(p)/$(2)-$(VERSION).xpkg ) \
+		$(1)/$(2):$(VERSION) || $(FAIL)
+	@$(OK) Pushed package $(1)/$(2):$(VERSION)
 xpkg.release.publish: xpkg.release.publish.$(1).$(2)
 
 xpkg.release.promote.$(1).$(2):
